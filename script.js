af65f9de09e1c2f48f2a7e908f5d250f056a842f
arisg8870 <91750543+arisg8870@users.noreply.github.com>
2021-10-01 12:53:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "arisg"; // update the part BETWEEN the "quotes"
+let userName = "arisg8870"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
